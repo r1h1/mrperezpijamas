31112e595460ca9e0524ead799f3ea1a366b1a95
Daniel Rivas <de.rivasherrera@gmail.com>
2024-05-07 20:58:13 -0700

    Se finaliza módulo usuario crud

--- business/a/usuarioAdmin.js
+++ business/a/usuarioAdmin.js
@@ -236,8 +236,12 @@ const mostrarUsuarioTabla = (dataObtained) => {
                     continue;
                 }
                 else {
-                    let editarBtn = `<button class="btn btn-warning" data-id="${dataObtained[i].usuarioId}"><i class="fa-solid fa-pen-to-square"></i></button>`;
-                    let borrarBtn = `<button class="btn btn-danger" data-id="${dataObtained[i].usuarioId}"><i class="fa-solid fa-trash"></i></button>`;
+                    let editarBtn = `<button class="btn btn-warning" onclick="editarUsuario(${dataObtained[i].usuarioId},'${dataObtained[i].nombreCompleto}',
+                    '${dataObtained[i].direccion}','${dataObtained[i].telefono}','${dataObtained[i].email}','${dataObtained[i].ciudad}','${dataObtained[i].municipio}',
+                    '${dataObtained[i].pais}','${dataObtained[i].referencia}','${dataObtained[i].nit}',${dataObtained[i].rolId})"><i class="fa-solid fa-pen-to-square"></i></button>`;
+                    let borrarBtn = `<button class="btn btn-danger" onclick="borrarUsuario(${dataObtained[i].usuarioId},'${dataObtained[i].nombreCompleto}',
+                    '${dataObtained[i].direccion}','${dataObtained[i].telefono}','${dataObtained[i].email}','${dataObtained[i].ciudad}','${dataObtained[i].municipio}',
+                    '${dataObtained[i].pais}','${dataObtained[i].referencia}','${dataObtained[i].nit}',${dataObtained[i].rolId})"><i class="fa-solid fa-trash"></i></button>`;
                     table.row.add([
                         dataObtained[i].nombreCompleto,
                         dataObtained[i].direccion,
@@ -260,6 +264,198 @@ const mostrarUsuarioTabla = (dataObtained) => {
         }
     } catch (err) {
         console.log(err);
+    }
+}
+
+
+const borrarUsuario = (usuarioId, nombreCompleto, direccion, telefono, email, ciudad, municipio, pais, referencia, nit, rolId) => {
+    try {
+        if (usuarioId != "" && nombreCompleto != "" && direccion != ""
+            && telefono != "" && email != "" && ciudad != ""
+            && municipio != "" && pais != "" && referencia != "" && nit != ""
+            && rolId != "") {
+            let myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            myHeaders.append("Authorization", "Bearer " + sessionStorage.getItem("tkn"));
+
+            var raw = JSON.stringify({
+                "usuarioId": usuarioId,
+                "nombreCompleto": nombreCompleto,
+                "direccion": direccion,
+                "telefono": telefono,
+                "email": email,
+                "ciudad": ciudad,
+                "municipio": municipio,
+                "pais": pais,
+                "referencia": referencia,
+                "nit": nit,
+                "rolId": rolId,
+                "estado": 0
+            });
+
+            var requestOptions = {
+                method: 'PUT',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+
+            fetch(UsuarioRoute, requestOptions)
+                .then(response => response.json())
+                .then(dataObtained => showData(dataObtained))
+                .catch(error => err = error);
+
+            const showData = (dataObtained) => {
+                if (dataObtained.isSuccess === true) {
+                    try {
+                        Toast.fire({
+                            icon: 'success',
+                            title: 'Completado con éxito.'
+                        });
+                        getUsuario();
+                    }
+                    catch (err) {
+                        Toast.fire({
+                            icon: 'warning',
+                            title: 'Se ha generado un error interno ' + err
+                        });
+                    }
+                }
+                else {
+                    Toast.fire({
+                        icon: 'warning',
+                        title: 'No se pudo completar el proceso, informe al administrador.'
+                    });
+                }
+            }
+        }
+        else {
+            Toast.fire({
+                icon: 'warning',
+                title: 'No se pudo obtener el dato a borrar.'
+            });
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+const editarUsuario = (usuarioId, nombreCompleto, direccion, telefono, email, ciudad, municipio, pais, referencia, nit, rolId) => {
+    try {
+        document.getElementById('idUsuario').value = usuarioId;
+        document.getElementById('nombreCompletoUsuario').value = nombreCompleto;
+        document.getElementById('direccionUsuario').value = direccion;
+        document.getElementById('telefonoUsuario').value = telefono;
+        document.getElementById('correoElectronicoUsuario').value = email;
+        document.getElementById('ciudadUsuario').value = ciudad;
+        document.getElementById('municipioUsuario').value = municipio;
+        document.getElementById('paisUsuario').value = pais;
+        document.getElementById('nitUsuario').value = nit;
+        document.getElementById('referenciaUsuario').value = referencia;
+        document.getElementById('rolUsuario').value = rolId;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
+const editarUsuarioLuegoDeEdicion = () => {
+    try {
+        let usuarioId = document.getElementById('idUsuario').value;
+        let nombreCompleto = document.getElementById('nombreCompletoUsuario').value;
+        let direccionUsuario = document.getElementById('direccionUsuario').value;
+        let telefonoUsuario = document.getElementById('telefonoUsuario').value;
+        let correoElectronicoUsuario = document.getElementById('correoElectronicoUsuario').value;
+        let ciudadUsuario = document.getElementById('ciudadUsuario').value;
+        let municipioUsuario = document.getElementById('municipioUsuario').value;
+        let paisUsuario = document.getElementById('paisUsuario').value;
+        let nitUsuario = document.getElementById('nitUsuario').value;
+        let referenciaUsuario = document.getElementById('referenciaUsuario').value;
+        let rolUsuario = document.getElementById('rolUsuario').value;
+
+        if (usuarioId != "" && nombreCompleto != "" && direccionUsuario != ""
+            && telefonoUsuario != "" && correoElectronicoUsuario != "" && ciudadUsuario != ""
+            && municipioUsuario != "" && paisUsuario != "" && referenciaUsuario != "" && nitUsuario != ""
+            && rolUsuario != "") {
+            let myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            myHeaders.append("Authorization", "Bearer " + sessionStorage.getItem("tkn"));
+
+            var raw = JSON.stringify({
+                "usuarioId": usuarioId,
+                "nombreCompleto": nombreCompleto,
+                "direccion": direccionUsuario,
+                "telefono": telefonoUsuario,
+                "email": correoElectronicoUsuario,
+                "ciudad": ciudadUsuario,
+                "municipio": municipioUsuario,
+                "pais": paisUsuario,
+                "referencia": referenciaUsuario,
+                "nit": nitUsuario,
+                "rolId": rolUsuario,
+                "estado": 1
+            });
+
+            var requestOptions = {
+                method: 'PUT',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+
+            fetch(UsuarioRoute, requestOptions)
+                .then(response => response.json())
+                .then(dataObtained => showData(dataObtained))
+                .catch(error => err = error);
+
+            const showData = (dataObtained) => {
+                if (dataObtained.isSuccess === true) {
+                    try {
+                        Toast.fire({
+                            icon: 'success',
+                            title: 'Completado con éxito.'
+                        });
+                        getUsuario();
+                        document.getElementById('idUsuario').value = '';
+                        document.getElementById('nombreCompletoUsuario').value = '';
+                        document.getElementById('direccionUsuario').value = '';
+                        document.getElementById('telefonoUsuario').value = '';
+                        document.getElementById('correoElectronicoUsuario').value = '';
+                        document.getElementById('ciudadUsuario').value = '';
+                        document.getElementById('municipioUsuario').value = '';
+                        document.getElementById('paisUsuario').value = '';
+                        document.getElementById('nitUsuario').value = '';
+                        document.getElementById('referenciaUsuario').value = '';
+                        document.getElementById('rolUsuario').value = '';
+                    }
+                    catch (err) {
+                        Toast.fire({
+                            icon: 'warning',
+                            title: 'Se ha generado un error interno ' + err
+                        });
+                    }
+                }
+                else {
+                    Toast.fire({
+                        icon: 'warning',
+                        title: 'No se pudo completar el proceso, informe al administrador.'
+                    });
+                }
+            }
+        }
+        else {
+            Toast.fire({
+                icon: 'warning',
+                title: 'Para editar, primero debes seleccionar un registro.'
+            });
+        }
+    }
+    catch (error) {
+        console.log(error);
     }
 }
 
